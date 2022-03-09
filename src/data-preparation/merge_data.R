adc69c471401f95cca109e05dd661f70bf936604
Paulina <97ambro@gmail.com>
2022-03-09 15:11:04 +0100

    Load datasets into R, merge on id, save merged data

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,9 +1,11 @@
 # Load datasets into R 
 df1 <- read.csv("./gen/data-preparation/input/dataset1.csv")
 df2 <- read.csv("./gen/data-preparation/input/dataset2.csv")
+df3 <- read.csv("./gen/data-preparation/input/dataset3.csv")
+df4 <- read.csv("./gen/data-preparation/input/dataset4.csv")
 
 # Merge on id
-df_merged <- merge(df1,df2,by="id")
+df_merged <- merge(df1,df2,df3,df4,by="id")
 
 # Save merged data
 save(df_merged,file="./gen/data-preparation/temp/data_merged.RData")
