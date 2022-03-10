9b533869a21292a3dd13a59991ea4a1dc53b2de8
Paulina <97ambro@gmail.com>
2022-03-10 18:09:45 +0100

    fix files numbers

--- src/data-preparation/update_input.R
+++ src/data-preparation/update_input.R
@@ -1,5 +1,5 @@
 # Copy the raw data into input folder 
 file.copy("./data/dataset1/dataset1.csv","./gen/data-preparation/input/dataset1.csv")
 file.copy("./data/dataset2/dataset2.csv","./gen/data-preparation/input/dataset2.csv")
-file.copy("./data/dataset1/dataset3.csv","./gen/data-preparation/input/dataset3.csv")
-file.copy("./data/dataset2/dataset4.csv","./gen/data-preparation/input/dataset4.csv")
+file.copy("./data/dataset3/dataset3.csv","./gen/data-preparation/input/dataset3.csv")
+file.copy("./data/dataset4/dataset4.csv","./gen/data-preparation/input/dataset4.csv")
