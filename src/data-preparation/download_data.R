245c2f2105b234fc46fcd49c808a3fa6e490613f
Paulina <97ambro@gmail.com>
2022-03-10 17:13:24 +0100

    All neccessary folders are created now and the numbers of folders were corrected

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,16 +1,18 @@
 # Download dataset 1 - 	Detailed Calendar Data for listings in New York City
+dir.create('./data/dataset1')
 download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/data/calendar.csv.gz','./data/dataset1/dataset1.csv')
 
 # Download dataset 2 - 	Summary information and metrics for listings in New York City
-download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/listings.csv','./data/dataset1/dataset2.csv')
+dir.create('./data/dataset2')
+download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/listings.csv','./data/dataset2/dataset2.csv')
 
 
 # Download dataset 3 - 	Neighbourhood list for geo filter. Sourced from city or open source GIS files.
 dir.create('./data/dataset3')
-download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/neighbourhoods.csv','./data/dataset1/dataset3.csv')
+download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/neighbourhoods.csv','./data/dataset3/dataset3.csv')
 
 
 # Download dataset 4 - 	NYPD Complaint Data Historic
 dir.create('./data/dataset4')
-download.file('https://data.cityofnewyork.us/api/views/qgea-i56i/rows.csv?accessType=DOWNLOAD','./data/dataset2/dataset4.csv')
+download.file('https://data.cityofnewyork.us/api/views/qgea-i56i/rows.csv?accessType=DOWNLOAD','./data/dataset4/dataset4.csv')
 
