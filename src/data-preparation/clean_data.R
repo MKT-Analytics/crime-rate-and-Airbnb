d35904a4bf1ef49d55543d710c49c911c14d12c5
Paulina <97ambro@gmail.com>
2022-03-22 21:36:31 +0100

    Added updated datasets paths

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,39 +1,38 @@
 # Load data 
-load("./gen/data-preparation/temp/dataset1.RData")
-load("./gen/data-preparation/temp/dataset2.RData")
-load("./gen/data-preparation/temp/dataset3.RData")
-load("./gen/data-preparation/temp/dataset4.RData")
+load("../../gen/data-preparation/temp/calendar.RData")
+load("../../gen/data-preparation/temp/listings.RData")
+load("../../gen/data-preparation/temp/neighbourhoods.RData")
+load("../../gen/data-preparation/temp/NYPD-complaint.RData")
 
 library(data.table)
 library(dplyr)
 
-#calendar; dates of listings with availability, price, min and max nights
-#does "calendar" need to be changed to "data_merged"?
-calendar$date =as.Date(calendar$date, format = "%Y-%m-%d")
+# Calendar; dates of listings with availability, price, min and max nights
+calendar$date = as.Date(calendar$date, format = "%Y-%m-%d")
 
-#per listing first and last availability date
+# Per listing first and last availability date
 unique_listings <- unique(calendar$listing_id)
 first_availabilty_listings <- aggregate(date ~ listing_id, calendar, function(x) min(x))
 last_availabilty_listings <- aggregate(date ~ listing_id, calendar, function(x) max(x))
 
-#read in crime data
-#select the relevant columns and place in a new dataframe
+# Read in crime data
+# Select the relevant columns and place in a new dataframe
 crime_sub <- crime[,c('CMPLNT_NUM', 'ADDR_PCT_CD', 'BORO_NM','CMPLNT_FR_DT','CMPLNT_TO_DT','CRM_ATPT_CPTD_CD','KY_CD','LAW_CAT_CD','OFNS_DESC','RPT_DT','Latitude','Longitude')]
 
-#Crimes where the location is not defined neither is a latitude or longitude given
+# Crimes where the location is not defined neither is a latitude or longitude given
 temp <- crime_sub[crime_sub$BORO_NM =='' & (is.na(crime_sub$Latitude) | is.na(crime_sub$Longitude)),]
-#delete those from the data
+# Delete those from the data
 crime_sub <- crime_sub[-c(as.numeric(row.names(temp))),]
 
 
-#retieve Longitudes and latitudes of the different neighbourhoods
+# Retieve longitudes and latitudes of the different neighbourhoods
 neighbourhoods_coordinates <- crime_sub[,c('BORO_NM','Latitude','Longitude')]
 neighbourhoods_coordinates <-na.omit(neighbourhoods_coordinates)
 neighbourhoods_lonlat = neighbourhoods_coordinates %>% group_by(BORO_NM) %>% summarise(min_lat = min(Latitude), max_lat =max(Latitude), min_lon= min(Longitude), max_lon= max(Longitude))
 neighbourhoods_lonlat <- neighbourhoods_lonlat[-c(1),]
 
 
-#for crime_sub[crime_sub$BORO_NM =='',] in crime_sub{
+# for crime_sub[crime_sub$BORO_NM =='',] in crime_sub{
 #  if (as.numeric(crime_sub$Longitude) < as.numeric(neighbourhoods_lonlat$max_lon) & crime_sub$Longitude > neighbourhoods_lonlat$min_lon & crime_sub$Latitude < neighbourhoods_lonlat$max_lat & crime_sub$Latitude > neighbourhoods_lonlat$min_lat){
 #    print(neighbourhoods_lonlat$BORO_NM)
 #  }
@@ -41,13 +40,13 @@ neighbourhoods_lonlat <- neighbourhoods_lonlat[-c(1),]
 
 crime_sub_notreplaced <-crime_sub[!(crime_sub$BORO_NM ==''),]
 
-#Change dates from characters to Date object
+# Change dates from characters to date object
 crime_sub$CMPLNT_FR_DT = as.Date(crime_sub$CMPLNT_FR_DT, format = "%m/%d/%Y")
 crime_sub$CMPLNT_TO_DT = as.Date(crime_sub$CMPLNT_TO_DT, format = "%m/%d/%Y")
 crime_sub$RPT_DT = as.Date(crime_sub$RPT_DT, format = "%m/%d/%Y")
 
 
-#look for outliers in the dates
+# Check for for outliers in the dates
 boxplot(crime_sub$CMPLNT_FR_DT, main = "Date of occurence for the reported event",
         xlab = "years",
         ylab = "date",
