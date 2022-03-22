5b320de1fbe29235b8fe57c60241d0f7f6248ab3
Paulina <97ambro@gmail.com>
2022-03-22 21:32:41 +0100

    Updated paths and datasets names

--- src/data-preparation/load_data.R
+++ src/data-preparation/load_data.R
@@ -1,11 +1,11 @@
-calendar <- read.csv("./gen/data-preparation/input/dataset1.csv")
-listings <- read.csv("./gen/data-preparation/input/dataset2.csv")
-neighbourhoods <- read.csv("./gen/data-preparation/input/dataset3.csv")
-crime_data <- read.csv("./gen/data-preparation/input/dataset4.csv")
+calendar <- read.csv("../../data/Inside-Airbnb/calendar.csv")
+listings <- read.csv("../../data/Inside-Airbnb/listings.csv")
+neighbourhoods <- read.csv("../../data/Inside-Airbnb/neighbourhoods.csv")
+crime_data <- read.csv("../../data/NYC-OpenData/NYPD-complaint.csv")
 
 
 # Save data
-save(calendar,file="./gen/data-preparation/temp/dataset1.RData")
-save(listings,file="./gen/data-preparation/temp/dataset2.RData")
-save(neighbourhoods,file="./gen/data-preparation/temp/dataset3.RData")
-save(crime_data,file="./gen/data-preparation/temp/dataset4.RData")
+save(calendar,file="../../gen/data-preparation/temp/calendar.RData")
+save(listings,file="../../gen/data-preparation/temp/listings.RData")
+save(neighbourhoods,file="../../gen/data-preparation/temp/neighbourhoods.RData")
+save(crime_data,file="../../gen/data-preparation/temp/NYPD-complaint.RData")
