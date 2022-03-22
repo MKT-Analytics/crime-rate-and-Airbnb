ec1d9e2ac99a4ac5b4d195bffecaa51821b7d77b
Paulina <97ambro@gmail.com>
2022-03-22 21:14:10 +0100

    Added the correct path /../../ & changed the number of data folders to 2 instead of 4 & changed datasets names to meaningful names

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,19 +1,17 @@
 dir.create('../../data')
 
 # Download dataset 1 - 	Detailed Calendar Data for listings in New York City
-dir.create('./data/dataset1')
-download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/data/calendar.csv.gz','./data/dataset1/dataset1.csv')
+dir.create('../../data/Inside-Airbnb')
+download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/data/calendar.csv.gz','../../data/Inside-Airbnb/calendar.csv')
 
 # Download dataset 2 - 	Summary information and metrics for listings in New York City
-dir.create('./data/dataset2')
-download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/listings.csv','./data/dataset2/dataset2.csv')
+download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/listings.csv','../../data/Inside-Airbnb/listings.csv')
 
 
 # Download dataset 3 - 	Neighbourhood list for geo filter. Sourced from city or open source GIS files.
-dir.create('./data/dataset3')
-download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/neighbourhoods.csv','./data/dataset3/dataset3.csv')
+download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/neighbourhoods.csv','../../data/Inside-Airbnb/neighbourhoods.csv')
 
 
 # Download dataset 4 - 	NYPD Complaint Data Historic
-dir.create('./data/dataset4')
-download.file('https://data.cityofnewyork.us/api/views/qgea-i56i/rows.csv?accessType=DOWNLOAD','./data/dataset4/dataset4.csv')
+dir.create('../../data/NYC-OpenData')
+download.file('https://data.cityofnewyork.us/api/views/qgea-i56i/rows.csv?accessType=DOWNLOAD','../../data/NYC-OpenData/NYPD-complaint.csv')
