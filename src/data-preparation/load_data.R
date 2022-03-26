03f7c7cc4058e65282f83c0db2d01982898639d7
Paulina <97ambro@gmail.com>
2022-03-26 21:59:29 +0100

    Pushing changes from Saad

--- src/data-preparation/load_data.R
+++ src/data-preparation/load_data.R
@@ -1,5 +1,6 @@
 calendar <- read.csv("../../data/Inside-Airbnb/calendar.csv")
 listings <- read.csv("../../data/Inside-Airbnb/listings.csv")
+listings_2 <- read.csv("../../data/Inside-Airbnb/listings_2.csv")
 neighbourhoods <- read.csv("../../data/Inside-Airbnb/neighbourhoods.csv")
 crime_data <- read.csv("../../data/NYC-OpenData/NYPD-complaint.csv")
 
@@ -8,5 +9,6 @@ crime_data <- read.csv("../../data/NYC-OpenData/NYPD-complaint.csv")
 dir.create('../../gen/data-preparation/temp/')
 save(calendar,file="../../gen/data-preparation/temp/calendar.RData")
 save(listings,file="../../gen/data-preparation/temp/listings.RData")
+save(listings_2,file="../../gen/data-preparation/temp/listings_2.RData")
 save(neighbourhoods,file="../../gen/data-preparation/temp/neighbourhoods.RData")
 save(crime_data,file="../../gen/data-preparation/temp/NYPD-complaint.RData")
