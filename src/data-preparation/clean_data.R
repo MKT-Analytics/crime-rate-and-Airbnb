be5928240295f9f3210585f4625d7d80d1d9e332
Paulina <97ambro@gmail.com>
2022-03-26 23:58:25 +0100

    Updating with new code

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,65 +1,55 @@
-# Load data 
+# Loading packages
+library(plyr)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(readr)
+library(data.table)
+
+# Import raw data
 load("../../gen/data-preparation/temp/calendar.RData")
 load("../../gen/data-preparation/temp/listings.RData")
+load("../../gen/data-preparation/temp/listings_2.RData")
 load("../../gen/data-preparation/temp/neighbourhoods.RData")
 load("../../gen/data-preparation/temp/NYPD-complaint.RData")
 
-library(data.table)
-library(dplyr)
 
-# Calendar; dates of listings with availability, price, min and max nights
-calendar$date = as.Date(calendar$date, format = "%Y-%m-%d")
+#Data exploration
+#Airbnb data
+head(listings)
+head(listings_2)
 
-# Per listing first and last availability date
-unique_listings <- unique(calendar$listing_id)
-first_availabilty_listings <- aggregate(date ~ listing_id, calendar, function(x) min(x))
-last_availabilty_listings <- aggregate(date ~ listing_id, calendar, function(x) max(x))
-
-# Read in crime data
-# Select the relevant columns and place in a new dataframe
-crime_sub <- crime[,c('CMPLNT_NUM', 'ADDR_PCT_CD', 'BORO_NM','CMPLNT_FR_DT','CMPLNT_TO_DT','CRM_ATPT_CPTD_CD','KY_CD','LAW_CAT_CD','OFNS_DESC','RPT_DT','Latitude','Longitude')]
-
-# Crimes where the location is not defined neither is a latitude or longitude given
-temp <- crime_sub[crime_sub$BORO_NM =='' & (is.na(crime_sub$Latitude) | is.na(crime_sub$Longitude)),]
-# Delete those from the data
-crime_sub <- crime_sub[-c(as.numeric(row.names(temp))),]
+unique(listings$neighbourhood_group) 
+neighbourhood_count <- listings %>% group_by(neighbourhood_group) %>% summarize(count=n())
+percentages <- round(neighbourhood_count$count/sum(neighbourhood_count$count)*100, 2)
+percentages <- paste0(percentages, "%")
+neighbourhood_count$neighbourhood_group <- paste0(neighbourhood_count$neighbourhood_group, percentages )
+pie(neighbourhood_count$count,neighbourhood_count$neighbourhood_group, main= "listings per neighbourhood")
 
 
-# Retieve longitudes and latitudes of the different neighbourhoods
-neighbourhoods_coordinates <- crime_sub[,c('BORO_NM','Latitude','Longitude')]
-neighbourhoods_coordinates <-na.omit(neighbourhoods_coordinates)
-neighbourhoods_lonlat = neighbourhoods_coordinates %>% group_by(BORO_NM) %>% summarise(min_lat = min(Latitude), max_lat =max(Latitude), min_lon= min(Longitude), max_lon= max(Longitude))
-neighbourhoods_lonlat <- neighbourhoods_lonlat[-c(1),]
+#Crime data 
+head(crime_data)
 
+crime_data<- crime_data[,c('CMPLNT_NUM', 'ADDR_PCT_CD', 'BORO_NM','CMPLNT_FR_DT','CMPLNT_TO_DT','CRM_ATPT_CPTD_CD','KY_CD','LAW_CAT_CD','OFNS_DESC','RPT_DT','Latitude','Longitude')]
+head(crime_data)
 
-# for crime_sub[crime_sub$BORO_NM =='',] in crime_sub{
-#  if (as.numeric(crime_sub$Longitude) < as.numeric(neighbourhoods_lonlat$max_lon) & crime_sub$Longitude > neighbourhoods_lonlat$min_lon & crime_sub$Latitude < neighbourhoods_lonlat$max_lat & crime_sub$Latitude > neighbourhoods_lonlat$min_lat){
-#    print(neighbourhoods_lonlat$BORO_NM)
-#  }
-#}
+crime_data$CMPLNT_FR_DT = as.Date(crime_data$CMPLNT_FR_DT, format = "%m/%d/%Y")
+crime_data$CMPLNT_TO_DT = as.Date(crime_data$CMPLNT_TO_DT, format = "%m/%d/%Y")
+crime_data$RPT_DT = as.Date(crime_data$RPT_DT, format = "%m/%d/%Y")
 
-crime_sub_notreplaced <-crime_sub[!(crime_sub$BORO_NM ==''),]
-
-# Change dates from characters to date object
-crime_sub$CMPLNT_FR_DT = as.Date(crime_sub$CMPLNT_FR_DT, format = "%m/%d/%Y")
-crime_sub$CMPLNT_TO_DT = as.Date(crime_sub$CMPLNT_TO_DT, format = "%m/%d/%Y")
-crime_sub$RPT_DT = as.Date(crime_sub$RPT_DT, format = "%m/%d/%Y")
-
-
-# Check for for outliers in the dates
-boxplot(crime_sub$CMPLNT_FR_DT, main = "Date of occurence for the reported event",
+boxplot(crime_data$CMPLNT_FR_DT, main = "Date of occurence for the reported event",
         xlab = "years",
         ylab = "date",
         horizontal = TRUE,
         notch = TRUE
 )
-boxplot(crime_sub$CMPLNT_TO_DT, main = "Ending Date of occurence for the reported event",
+boxplot(crime_data$CMPLNT_TO_DT, main = "Ending Date of occurence for the reported event",
         xlab = "years",
         ylab = "date",
         horizontal = TRUE,
         notch = TRUE
 )
-boxplot(crime_sub$RPT_DT, main = "Date event was reported to police",
+boxplot(crime_data$RPT_DT, main = "Date event was reported to police",
         xlab = "years",
         ylab = "date",
         horizontal = TRUE,
@@ -67,7 +57,31 @@ boxplot(crime_sub$RPT_DT, main = "Date event was reported to police",
 )
 
 
+crimes_per_neighbourhood <- crime_data %>% group_by(BORO_NM) %>% summarize(count=n())
+percentages <- round(crimes_per_neighbourhood$count/sum(crimes_per_neighbourhood$count)*100, 2)
+percentages <- paste0(percentages, "%")
+crimes_per_neighbourhood$BORO_NM <- paste0(crimes_per_neighbourhood$BORO_NM, percentages )
+pie(crimes_per_neighbourhood$count,crimes_per_neighbourhood$BORO_NM, main= "crimes per neighbourhood")
 
-# Save cleaned data
+crime_cat <- crime_data %>% 
+  group_by(BORO_NM, LAW_CAT_CD) %>% 
+  summarize(count = n())
+ggplot(crime_cat,                                     
+       aes(x = BORO_NM,
+           y = count,
+           fill = LAW_CAT_CD)) +
+  geom_bar(stat = "identity",
+           position = "dodge")+ ggtitle("Crime categories per neighbourhood") +
+  xlab("neighbourhood") + ylab("Count") +labs(fill="Category")
+
+
+#Data Cleaning
+#number of missing neighbourhoods
+crime_cat
+sum(filter(crime_cat, BORO_NM=="")[3])
+crime_data_cleaned <-crime_data[!(crime_data$BORO_NM ==''),]
+
+
+#store the cleaned data
 dir.create('../../gen/data-preparation/output/')
 save(df_cleaned,file="../../gen/data-preparation/output/data_cleaned.RData")
