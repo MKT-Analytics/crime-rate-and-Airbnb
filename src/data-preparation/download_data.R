8348c9ebbf9488a2cee4dd5d6f22166785b5af7d
Paulina <97ambro@gmail.com>
2022-03-26 22:00:04 +0100

    Pushing changes from Saad

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -5,13 +5,15 @@ dir.create('../../data/Inside-Airbnb')
 download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/data/calendar.csv.gz','../../data/Inside-Airbnb/calendar.csv')
 
 # Download dataset 2 - 	Summary information and metrics for listings in New York City
-download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/listings.csv','../../data/Inside-Airbnb/listings.csv')
+download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/data/listings.csv.gz','../../data/Inside-Airbnb/listings.csv')
 
+# Download dataset 3 - 	Summarized information and metrics for listings in New York City
+download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/listings.csv','../../data/Inside-Airbnb/listings_2.csv')
 
-# Download dataset 3 - 	Neighbourhood list for geo filter. Sourced from city or open source GIS files.
+# Download dataset 4 - 	Neighbourhood list for geo filter. Sourced from city or open source GIS files.
 download.file('http://data.insideairbnb.com/united-states/ny/new-york-city/2021-12-04/visualisations/neighbourhoods.csv','../../data/Inside-Airbnb/neighbourhoods.csv')
 
-
-# Download dataset 4 - 	NYPD Complaint Data Historic
+# Download dataset 5 - 	NYPD Complaint Data Historic
 dir.create('../../data/NYC-OpenData')
 download.file('https://data.cityofnewyork.us/api/views/qgea-i56i/rows.csv?accessType=DOWNLOAD','../../data/NYC-OpenData/NYPD-complaint.csv')
+
